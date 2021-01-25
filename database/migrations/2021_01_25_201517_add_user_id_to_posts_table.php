bdfa49dd1967eecda2111f51fc6a17b831b1359d
Zac <a19860101@gmail.com>
2021-01-25 22:06:41 +0800

    20210125

--- database/migrations/2021_01_25_201517_add_user_id_to_posts_table.php
+++ database/migrations/2021_01_25_201517_add_user_id_to_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddUserIsToPostsTable extends Migration
+class AddUserIdToPostsTable extends Migration
 {
     /**
      * Run the migrations.
