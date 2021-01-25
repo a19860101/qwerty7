85fb58835cfb3aeae54c127a7b84193c4128e7ee
Zac <a19860101@gmail.com>
2021-01-25 21:17:02 +0800

    20210125

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,6 +5,9 @@
         <div class="col-xl-8 col-10">
             <h3>{{$post->title}}</h3>
             <div>
+                作者: {{$post->user->name}}
+            </div>
+            <div>
                 分類: <span class="badge badge-secondary">
                    {{$post->category->title}}
                 </span>
@@ -15,12 +18,14 @@
             <div>最後更新時間 {{$post->updated_at}}</div>
             <hr>
             <a href="#" class="btn btn-outline-info" onclick="history.back()">回上頁</a>
+            @if($post->user->id == Auth::id())
             <form action="{{route('posts.destroy',['post'=>$post->id])}}" method="post" class="d-inline-block">
                 @csrf
                 @method('delete')
                 <input type="submit" class="btn btn-outline-danger" value="刪除" onclick="return confirm('確認刪除？')">
             </form>
             <a href="{{route('posts.edit',['post' => $post->id])}}" class="btn btn-outline-success">編輯</a>
+            @endif
         </div>
     </div>
 </div>
